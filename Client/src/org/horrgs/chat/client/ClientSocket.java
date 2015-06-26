dc6e15e811039c6d9ae4f8c49f8b2fdfdfdb6d80
Horrgs <horrgs34@gmail.com>
2015-06-26 01:52:18 -0400

    ClientSocket & MainMenu

---
+++ Client/src/org/horrgs/chat/client/ClientSocket.java
@@ -0,0 +1,7 @@
+package org.horrgs.chat.client;
+
+/**
+ * Created by Horrgs on 5/16/2015.
+ */
+public class ClientSocket {
+}
---
+++ Client/src/org/horrgs/chat/client/windows/MainMenu.java
@@ -0,0 +1,42 @@
+package org.horrgs.chat.client.windows;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Horrgs on 6/17/2015.
+ */
+public class MainMenu extends JFrame {
+    private JButton createAccount;
+    private JButton login;
+    public MainMenu() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+        setTitle("Chat - Main Menu");
+        setSize(600, 600);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        createAccount = new JButton("Create Account");
+        createAccount.addActionListener(new MainMenuListener());
+        add(createAccount, gbc);
+        login = new JButton("Login");
+        login.addActionListener(new MainMenuListener());
+        gbc.gridy = 1;
+        add(login, gbc);
+    }
+
+    private class MainMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            if(ev.getSource() == createAccount) {
+                setVisible(false);
+                new CreateAccount();
+            } else if(ev.getSource() == login) {
+                //TODO: login window.
+            }
+        }
+    }
+}
