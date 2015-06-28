88289ce05bd2bd6efb6b426bd841010885ced922
Horrgs <horrgs34@gmail.com>
2015-06-28 03:45:25 -0400

    Making a connecting w/ the server.

--- Client/src/org/horrgs/chat/client/Chat.java
+++ Client/src/org/horrgs/chat/client/Chat.java
@@ -1,7 +1,13 @@
 package org.horrgs.chat.client;
 
+import org.horrgs.chat.client.windows.CreateAccount;
+
 /**
  * Created by Horrgs on 5/15/2015.
  */
 public class Chat {
+
+    public static void main(String[] args) {
+        new CreateAccount();
+    }
 }
--- Client/src/org/horrgs/chat/client/ClientSocket.java
+++ Client/src/org/horrgs/chat/client/ClientSocket.java
@@ -1,7 +1,26 @@
 package org.horrgs.chat.client;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by Horrgs on 5/16/2015.
  */
 public class ClientSocket {
+    private Socket socket;
+    public BufferedReader bufferedReader;
+    public PrintWriter printWriter;
+
+    public void connect() {
+        try {
+            socket = new Socket("127.0.0.1", 5000);
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+            printWriter = new PrintWriter(socket.getOutputStream());
+            System.out.println("Connection established.");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Issues connecting to 127.0.0.1 on port 5000");
+        }
+    }
 }
