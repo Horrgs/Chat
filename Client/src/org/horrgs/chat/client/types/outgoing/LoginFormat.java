1af6e68b7ec0e0e9924de2d35a0a68bf97aaba9d
Horrgs <horrgs34@gmail.com>
2015-07-01 01:27:14 -0400

    Email support and some getter methods

--- Client/src/org/horrgs/chat/client/types/outgoing/LoginFormat.java
+++ Client/src/org/horrgs/chat/client/types/outgoing/LoginFormat.java
@@ -7,14 +7,19 @@ import org.horrgs.chat.client.types.RequestType;
  */
 public class LoginFormat {
     private RequestType requestType;
+    private String email;
     private String username;
     private String password;
-    //TODO: needs support of email.
-    public LoginFormat(RequestType requestType, String username, String password) {
+    public LoginFormat(RequestType requestType, String email, String username, String password) {
+        this.email = email;
         this.requestType = requestType;
         this.username = username;
         this.password = password;
     }
+
+    public RequestType getType() { return requestType; }
+
+    public String getEmail() { return  email; }
     
     public RequestType getRequestType() {
         return requestType;
--- Server/src/org/horrgs/chat/server/types/incoming/LoginFormat.java
+++ Server/src/org/horrgs/chat/server/types/incoming/LoginFormat.java
@@ -7,14 +7,17 @@ import org.horrgs.chat.server.types.RequestType;
  */
 public class LoginFormat {
     private RequestType requestType;
+    private String email;
     private String username;
     private String password;
-    //TODO: needs support of email.
-    public LoginFormat(RequestType requestType, String username, String password) {
+    public LoginFormat(RequestType requestType, String email, String username, String password) {
+        this.email = email;
         this.requestType = requestType;
         this.username = username;
         this.password = password;
     }
+
+    public String getEmail() { return email; }
     
     public RequestType getRequestType() {
         return requestType;
