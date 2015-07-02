4e32af777b463005298e50c3fb688cfc16c02123
Horrgs <horrgs34@gmail.com>
2015-07-02 02:07:06 -0400

    User class added to client.

---
+++ Client/src/org/horrgs/chat/client/windows/User.java
@@ -0,0 +1,32 @@
+package org.horrgs.chat.client.windows;
+
+/**
+ * Created by Horrgs on 7/1/2015.
+ */
+public class User {
+    private String email,username,password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
