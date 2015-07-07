d939e4cb888c6e89c09fa80c747baf8bf500f69c
Horrgs <horrgs34@gmail.com>
2015-07-06 23:51:35 -0400

    RankManager -> Rank

---
+++ Client/src/org/horrgs/chat/client/users/Rank.java
@@ -0,0 +1,29 @@
+package org.horrgs.chat.client.users;
+
+/**
+ * Created by Horrgs on 7/6/2015.
+ */
+public enum Rank {
+    USER("USER"),
+    MODERATOR("MODERATOR"),
+    ADMINISTRATOR("ADMINISTRATOR");
+
+    private String name;
+
+    private Rank(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Rank getByName(String name) {
+        for(Rank rank : Rank.values()) {
+            if(rank.getName().equals(name)) {
+                return rank;
+            }
+        }
+        return null;
+    }
+}
